7348f84f3d2cbc784791e42ef17a0058721e9330
jr1412594 <jr.medina1412@gmail.com>
2020-11-06 10:47:06 -0700

    migrate for dogs

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,15 @@
 class DogsController < ApplicationController
+    def index
+        @dogs = Dog.all 
+        render json: @dogs
+    end
+
+    def create
+        @dog = Dog.create({
+            name: params[:name],
+            age: params[:age]
+        })
+
+        render json: {dog: @dog}, status: :created
+    end
 end
